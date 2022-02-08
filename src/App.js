b0b81984faa678e5721f39bd1a8e7fc06b281613
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-08 12:49:33 +0800

    updated .js file and removed placeholder react files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import logo from './comingSoon.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Chrysalis Portfolio</h1>
+        <img src={logo}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Website is under construction. Please check back again.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
