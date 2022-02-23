948de57415960343f9a245f389038eecfc8f2ab1
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-23 23:27:49 +0800

    Updated image assets folder

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,14 +1,15 @@
+import React from 'react';
+
 export default function Contact() {
-    return (
-      <div className="Contact">
-          <h1>Get In Touch</h1>
-        <div className="MainDescription">
-          <p>Insert description</p>
-        </div>
-        <div className="SocialIcons">
-          
-        </div>
+  return (
+    <div className="Contact">
+      <h1>Get In Touch</h1>
+      <div className="MainDescription">
+        <p>Insert description</p>
       </div>
-    );
-  }
-  
+      <div className="SocialIcons">
+
+      </div>
+    </div>
+  );
+}
