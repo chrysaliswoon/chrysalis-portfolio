653796b1669d090f9f4b1ec8e84433897714d6cb
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-25 21:55:46 +0800

    Moved the files so it's easier to identify components and routes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,7 +8,10 @@ export default function Contact() {
         <p>Follow me on my social media or drop me an email!</p>
       </div>
       <div className="SocialIcons">
+        <img src="" alt="LinkedIn Logo"></img>
         <img src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Components/Image%20Assets/twitter.png?raw=true" alt="Twitter Logo"></img>
+        <img src="" alt="Github Logo"></img>
+        <img src="" alt="Email Logo"></img>
       </div>
     </div>
   );
