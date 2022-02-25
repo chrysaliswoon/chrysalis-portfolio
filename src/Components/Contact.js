725272582a0ec8f87ea47adc48b1c6abb1be967c
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-25 22:35:53 +0800

    Updated links in Contact Component file and checked if links are working

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,10 +8,10 @@ export default function Contact() {
         <p>Follow me on my social media or drop me an email!</p>
       </div>
       <div className="SocialIcons">
-        <img src="" alt="LinkedIn Logo"></img>
-        <img src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Components/Image%20Assets/twitter.png?raw=true" alt="Twitter Logo"></img>
-        <img src="" alt="Github Logo"></img>
-        <img src="" alt="LinkedIn Logo"></img>
+        <a href="https://www.linkedin.com/in/chrysalis-woon-38376198/"><img src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/linkedin.png?raw=true" alt="LinkedIn Logo"></img></a>
+        <a href="https://twitter.com/chrysaliswoon"><img src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/twitter.png?raw=true" alt="Twitter Logo"></img></a>
+        <a href="https://github.com/chrysaliswoon"><img src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/github.png?raw=true" alt="Github Logo"></img></a>
+        <a href = "mailto:chrysaliswoon@gmail.com"><img src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/email.png?raw=true" alt="Email Logo"></img></a>
       </div>
     </div>
   );
