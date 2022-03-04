8574809da5072b8abf4218e29ebde76c0b4476d0
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-03-05 01:19:31 +0800

    Used Bootstrap Loader for Quotes page

--- src/Pages/Loader/Loader.js
+++ src/Pages/Loader/Loader.js
@@ -1,43 +1,51 @@
-import "./Loader.css"
-import React, {useState, useEffect} from 'react'
-import { useNavigate } from 'react-router'
-import axios from "axios"
-
+import "./Loader.css";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import axios from "axios";
+import { RingLoader } from 'react-spinners';
 
 export default function LoadingPage() {
-  const [isLoading, setLoading] = useState(true)
-  const [quotes, setQuotes] = useState()
-  const navigate = useNavigate()
-
-
-  useEffect(() => {
-    axios.get("https://api.quotable.io/random?maxLength=50")
-    .then(response => {
-      setQuotes(response.data)
-      setLoading(false)
-    })
-  }, [])
+  const [isLoading, setLoading] = useState(true);
+  const [quotes, setQuotes] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-        // console.log('1 second passed')
-        navigate('/home')
-    }, 3000)
-    return () => clearTimeout(timer)
-}, [])
+      // console.log('1 second passed')
+      navigate("/home");
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://api.quotable.io/random?maxLength=50")
+      .then((response) => {
+        setQuotes(response.data);
+        setLoading(false);
+      });
+  }, []);
 
   if (isLoading) {
-    return <div className="App">Loading...</div>
-  }
-
     return (
-        <div className="Header">
-          <div>
-              <li key={quotes._id}>
-                  <p>"{quotes.content}"</p> 
-                  - {quotes.author}
-              </li>
-          </div>
-        </div>
+      <div className='sweet-loading'>
+        <RingLoader
+          color={'#123abc'} 
+        />
+      </div>
     );
   }
+
+  return (
+    <div className="Quote">
+      <div className="Header">
+        <blockquote className="blockquote text-center text-success">
+          <li key={quotes._id}>
+            <p>"{quotes.content}"</p>
+            <footer className="blockquote-footer">- {quotes.author}</footer>
+          </li>
+        </blockquote>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Welcome/WelcomeUser.js
+++ src/Pages/Welcome/WelcomeUser.js
@@ -1,23 +1,22 @@
-import "./WelcomeUser.css"
-import React, {useEffect, useState} from 'react'
-import { useNavigate } from 'react-router'
-import Form from '../../Components/Form/Form'
+import "./WelcomeUser.css";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import Form from "../../Components/Form/Form";
 
-export default function WelcomeUser({userInput}){
-    const navigate = useNavigate()
+export default function WelcomeUser({ userInput }) {
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // console.log('1 second passed')
+      navigate("/loading");
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            // console.log('1 second passed')
-            navigate('/loading')
-        }, 3000)
-        return () => clearTimeout(timer)
-    }, [])
-
-    return (
-        <div>
+  return (
+    <div>
         <p>Hi {userInput}! It's nice to meet you!</p>
-        </div>
-    )
+    </div>
+  );
 }
