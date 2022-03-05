80d8088fd3d7656133ac18d675d4b9c05e6efa43
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-03-05 17:18:36 +0800

    Updated CSS for homepage

--- src/Pages/Intro/Intro.js
+++ src/Pages/Intro/Intro.js
@@ -2,20 +2,31 @@ import "./Intro.css";
 import { useNavigate } from "react-router";
 import { useState } from "react";
 import Form from "../../Components/Form/Form";
-import ParticlesBackground from "../../Components/Particles/Particles"
+import ParticlesBackground from "../../Components/Particles/Particles";
 
 export default function Introduction() {
   const navigate = useNavigate();
-  const [name, setName] = useState("")
+  const [name, setName] = useState("");
 
   return (
     <div className="bg text-center">
       <ParticlesBackground />
       <div className="centered">
         <h1 className="firstLine">WELCOME!</h1>
-        <p className="secondLine">Glad you could stop by! What's your name?</p>
-          <Form name={name} setName={setName}/>
-          <button className = "btn" onClick={() => navigate("/welcome", {state:name})}>Submit</button>
+        <p className="secondLine">
+          Glad you could stop by! How do I address you?
+        </p>
+        <div className="bottom">
+          <Form name={name} setName={setName} />
+          <button
+            className="btn"
+            onClick={() => navigate("/welcome", { state: name })}
+          >
+            Submit
+          </button>
+        </div>
+        <br />
+        <p><i>This information will not be recorded, and is for entertainment purposes.</i></p>
       </div>
     </div>
   );
