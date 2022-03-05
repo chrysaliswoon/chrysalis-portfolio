7036af42ac0fb3d944881cae3e0a122b86b0e050
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-03-05 16:41:47 +0800

    Update value for Particles.js

--- src/Components/Particles/Particles.js
+++ src/Components/Particles/Particles.js
@@ -78,7 +78,7 @@ export default function ParticlesBackground() {
               enable: true,
               area: 1000,
             },
-            value: 80,
+            value: 40,
           },
           opacity: {
             value: 0.5,
