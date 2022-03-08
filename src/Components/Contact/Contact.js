ce7e415cf4032f4464386acab0179c85536572d9
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-03-08 22:27:29 +0800

    Updated Social Media Icons CSS - Unable to put them in a row

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ContactForm from './ContactForm'
+import "./Contact.css";
+import React from "react";
+import ContactForm from "./ContactForm";
 
 export default function Contact() {
   return (
@@ -9,13 +10,41 @@ export default function Contact() {
         <p>Follow me on my social media or drop me an email!</p>
       </div>
       <div className="SocialIcons">
-        <a href="https://www.linkedin.com/in/chrysalis-woon-38376198/"><img src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/linkedin.png?raw=true" alt="LinkedIn Logo"></img></a>
-        <a href="https://twitter.com/chrysaliswoon"><img src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/twitter.png?raw=true" alt="Twitter Logo"></img></a>
-        <a href="https://github.com/chrysaliswoon"><img src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/github.png?raw=true" alt="Github Logo"></img></a>
-        <a href = "mailto:chrysaliswoon@gmail.com"><img src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/email.png?raw=true" alt="Email Logo"></img></a>
+        <li className="socialIcons">
+          <a href="https://www.linkedin.com/in/chrysalis-woon-38376198/">
+            <img
+              src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/linkedin.png?raw=true"
+              alt="LinkedIn Logo"
+            ></img>
+          </a>
+        </li>
+        <li className="socialIcons">
+          <a href="https://twitter.com/chrysaliswoon">
+            <img
+              src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/twitter.png?raw=true"
+              alt="Twitter Logo"
+            ></img>
+          </a>
+        </li>
+        <li className="socialIcons">
+          <a href="https://github.com/chrysaliswoon">
+            <img
+              src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/github.png?raw=true"
+              alt="Github Logo"
+            ></img>
+          </a>
+        </li>
+        <li className="socialIcons">
+          <a href="mailto:chrysaliswoon@gmail.com">
+            <img
+              src="https://github.com/chrysaliswoon/chrysalis-portfolio/blob/main/src/Image%20Assets/email.png?raw=true"
+              alt="Email Logo"
+            ></img>
+          </a>
+        </li>
       </div>
       <div className="ContactForm">
-        <ContactForm formSlug="chrysaliswoon"/>
+        <ContactForm formSlug="chrysaliswoon" />
       </div>
     </div>
   );
