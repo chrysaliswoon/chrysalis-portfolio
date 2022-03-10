75dc476ea4fe0a70403ed5e38bbe0830a3990a4c
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-03-10 14:53:47 +0800

    installed react-spring parallax and used it on homepage
    Buttons and links are no changing the state

--- src/Pages/HomePage/homePage.js
+++ src/Pages/HomePage/homePage.js
@@ -1,4 +1,4 @@
-import "./homePage.css";
+import styles from "./homePage.css";
 import AboutMe from "../../Components/AboutMe/AboutMe";
 import Experience from "../../Components/Experiences/Experience";
 import Work from "../../Components/Projects/Projects";
@@ -6,46 +6,42 @@ import Contact from "../../Components/Contact/Contact";
 import NavBar from "../../Components/NavigationBar/NavBar";
 import Introduction from "../../Components/Introduction/Introduction";
 import Footer from "../../Components/Footer/Footer"
-// import SimpleForm from "../../Components/ChatBot/Chatbot";
+import {Parallax, ParallaxLayer} from '@react-spring/parallax'
 
 export default function Website() {
   // console.log(process.env.REACT_APP_LOAD_INTRO)
 
   return (
     // Top Navigation Bar Section //
-    <>
-      <nav id="Navigation">
-        <NavBar />
-      </nav>
+    <div>
+    <div className={styles.background}>
 
-      {/* Main Page Section */}
-      <section id="Introduction">
-        <Introduction />
-      </section>
+      <Parallax pages = {5}>
+        <ParallaxLayer offset={0} speed={0.5}>
+        <section id="Introduction"><Introduction /></section>
+      </ParallaxLayer>
 
-      {/* About Me Page Section */}
-      <section id="AboutMe">
-        <AboutMe />
-      </section>
+      <ParallaxLayer sticky={{start: 0, end: 5}}>
+      <nav id="Navigation"><NavBar /></nav>
+      </ParallaxLayer>
 
-      {/* My Experiences Page Section */}
-      <section id="Experience">
-        <Experience />
-      </section>
+      <ParallaxLayer offset={1.0} speed={0.5}>
+        <section id="AboutMe"><AboutMe /></section>
+      </ParallaxLayer>
 
-      {/* My Projects Page Section */}
-      <section id="Work">
-        <Work />
-      </section>
+      <ParallaxLayer offset={2.0} speed={0.5}>
+        <section id="Experience"><Experience /></section>
+      </ParallaxLayer>
 
-      {/* Contact Page Section  */}
-      <section id="Contact">
-        <Contact />
-        {/* <SimpleForm /> */}
-      </section>
-      <section id="Footer">
-      <Footer />
-      </section>
-    </>
+      <ParallaxLayer offset={3.0} speed={0.5}>
+        <section id="Work"><Work /></section>
+      </ParallaxLayer>
+
+      <ParallaxLayer sticky={{start: 4, end: 3}}>
+    <section id="Contact"><Contact /></section>
+      </ParallaxLayer>
+      </Parallax>
+    </div>
+    </div>
   );
 }
