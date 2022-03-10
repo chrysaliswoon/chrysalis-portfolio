955402733ed14eea31118f4f8de953b5a1f5e32b
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-03-10 21:40:49 +0800

    fixed projects page css

--- src/Pages/HomePage/homePage.js
+++ src/Pages/HomePage/homePage.js
@@ -1,48 +1,50 @@
-import homeStyle from "./homePage.module.css"
+import homeStyle from "./homePage.module.css";
 import AboutMe from "../../Components/AboutMe/AboutMe";
 import Experience from "../../Components/Experiences/Experience";
-import Work from "../../Components/Projects/Projects";
+import Work from "../../Components/Projects/projects";
 import Contact from "../../Components/Contact/Contact";
 import NavBar from "../../Components/NavigationBar/NavBar";
 import Introduction from "../../Components/Introduction/Introduction";
-import Footer from "../../Components/Footer/Footer"
-import {Parallax, ParallaxLayer} from '@react-spring/parallax'
+import Footer from "../../Components/Footer/Footer";
 
 export default function Website() {
-  // console.log(process.env.REACT_APP_LOAD_INTRO)
-
   return (
-    // Top Navigation Bar Section //
     <div>
-    <div className={homeStyle.background}>
-
-      {/* <Parallax pages = {4}>
-      <ParallaxLayer sticky={{start: 0, end: 4.5}}> */}
-      <nav id={homeStyle.Navigation}><NavBar /></nav>
-      {/* </ParallaxLayer>
-
-        <ParallaxLayer offset={0} speed={0.5}> */}
-        <section id={homeStyle.Introduction}><Introduction /></section>
-      {/* </ParallaxLayer>
-
-      <ParallaxLayer offset={1} speed={0.5}> */}
-        <section id={homeStyle.AboutMe}><AboutMe /></section>
-      {/* </ParallaxLayer>
-
-      <ParallaxLayer offset={2} speed={0.5}> */}
-        <section id={homeStyle.Experience}><Experience /></section>
-      {/* </ParallaxLayer>
-
-      <ParallaxLayer offset={3} speed={0.5}> */}
-        <section id={homeStyle.Work}><Work /></section>
-      {/* </ParallaxLayer>
-
-      <ParallaxLayer sticky={{start: 4, end: 4}}> */}
-    <section id={homeStyle.Contact}><Contact /></section>
-    <section id={homeStyle.Footer}><Footer /></section>
-      {/* </ParallaxLayer>
-      </Parallax> */}
-    </div>
+      <div className={homeStyle.background}>
+        <div className={homeStyle.navigation}>
+          <nav id="Navigation">
+            <NavBar />
+          </nav>
+        </div>
+        <div className={homeStyle.introduction}>
+          <section id="Introduction">
+            <Introduction />
+          </section>
+        </div>
+        <div className={homeStyle.aboutMe}>
+          <section id="AboutMe">
+            <AboutMe />
+          </section>
+        </div>
+        <div className={homeStyle.experience}>
+          <section id="Experience">
+            <Experience />
+          </section>
+        </div>
+        <div className={homeStyle.work}>
+          <section id="Work">
+            <Work />
+          </section>
+        </div>
+        <div className={homeStyle.contact}>
+          <section id="Contact">
+            <Contact />
+          </section>
+        </div>
+        <section id={homeStyle.footer}>
+          <Footer />
+        </section>
+      </div>
     </div>
   );
 }
