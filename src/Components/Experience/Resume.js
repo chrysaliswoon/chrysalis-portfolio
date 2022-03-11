92158a2b1ff976f633251046f1cedd6142ba4719
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-03-11 10:41:14 +0800

    fixed job experience portion

--- src/Components/Experience/Resume.js
+++ src/Components/Experience/Resume.js
@@ -1,35 +1,43 @@
-import resumeStyle from "./resume.module.css"
+import resumeStyle from "./resume.module.css";
 import React, { useState } from "react";
 import data from "../../Data/resumeData";
-import Experience from "./Experience";
-import JobDetails from "../Experience/JobDetails";
 
 export default function Resume() {
-  const [company, setCompany] = useState(data);
-  const [descriptionID, setDescriptionID] = useState(data[0]);
+  const [description, setDescription] = useState(data[0]);
+  const handleClick = (resumeID) => setDescription(resumeID);
 
-  const update = (details) => {
-    setDescriptionID(details);
-    console.log("clicked")
-  };
+  const companies = data.map((companyList) => (
+    <li
+      className={resumeStyle.jobs}
+      onClick={() => handleClick(companyList)}
+      style={{ cursor: "pointer" }}
+    >
+      {companyList.company}
+    </li>
+  ));
 
   return (
     <div className="Experience">
-    <h1>My Journey</h1>
-    <div className="Header">A Brief History of My Work</div>
-    <div className={resumeStyle.experienceList}>
-      <div style={{ display: "flex" }}>
-        <nav
-          style={{
-            borderRight: "solid 1px",
-          }}
-        >
-          <Experience experience = {company} handleClick={update}/>
-        </nav>
-        <div className={resumeStyle.experienceDescription}>
-        <JobDetails experience = {descriptionID} />
+      <h1>My Journey</h1>
+      <div className="Header">A Brief History of My Work</div>
+
+      <div className={resumeStyle.experienceList}>
+        <div style={{ display: "flex" }}>
+          <nav className={resumeStyle.navigation}>
+            {companies}
+          </nav>
+          <div className={resumeStyle.experienceDescription}>
+            <h5>
+              {description.jobTitle} {description.company}
+            </h5>
+            <p>{description.duration}</p>
+            <ul>
+              <li>{description.point1}</li>
+              <li>{description.point2}</li>
+              <li>{description.point3}</li>
+            </ul>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
